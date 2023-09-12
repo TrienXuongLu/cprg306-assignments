725758760505fb73ff93e39890079758dfe53323
Trien Xuong Lu <lutrienxuong@gmail.com>
2023-09-12 14:09:27 -0600

    commit week2

---
+++ app/studentinfo.js
@@ -0,0 +1,9 @@
+export default function StudentInfo() {
+    return (
+        <>
+            <div>Name: <p>Trien Xuong Lu</p></div>
+            <div>Course: <p>CPRG 306 A</p></div>
+            <div>GitHub Repository: <p>https://github.com/TrienXuongLu/cprg306-assignments</p></div>
+        </>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from "../studentinfo";
+
+export default function Home() {
+    return (
+        <>
+            <h1>My Shopping List</h1>
+            <StudentInfo />
+            
+        </>
+    )
+}
